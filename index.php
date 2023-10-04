3b5f9f36209a0cdafe407c19ba2df39a4b616632
Adem Ahmetovic <ademahmetovic2002@gmail.com>
2023-10-04 14:36:27 +0200

    implement selectQuery

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = ''; // IMPLEMENT ME (ADEM)
+$selectQuery = 'SELECT * FROM todo'; // IMPLEMENT ME (ADEM)
 $items = $db->query($selectQuery);
 ?>
 
